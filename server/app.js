e2469fb5314b2f29c06b2e5297ce3a6d64309c3c
Arthur Wagenknecht <oarthurdev@gmail.com>
2024-09-18 19:44:48 -0400

    Added vercej.json

--- server/app.js
+++ server/app.js
@@ -7,42 +7,40 @@ const itemRoutes = require('./routes/ItemRoutes');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+// Middleware
+app.use(bodyParser.json());
+app.use(cookieParser(process.env.SESSION_SECRET));
+app.use(cors({
+    origin: '*', // ou especifique seu domínio frontend em produção
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 
+// Sessão
 const sessionConfig = {
     id: "1",
     secret: "1234",
     resave: false,
     saveUninitialized: false,
     cookie: {
-    maxAge: 1000 * 60 * 30,
-    sameSite: true,
-    secure: false, // ajustar para false em conexoes HTTP
-    sameSite: true,
-    httpOnly: true
+        maxAge: 1000 * 60 * 30,
+        sameSite: true,
+        secure: false, // Ajuste para true em produção com HTTPS
+        httpOnly: true
     }
 };
-
 app.use(session(sessionConfig));
 
-app.use(cookieParser(process.env.SESSION_SECRET));
-// Configure bodyParser and other middleware
-app.use(bodyParser.json());
+// Rotas da API
+app.use('/api', itemRoutes);
 
-// Configure CORS
-const corsOptions = {
-    origin: '*', // Replace with your frontend domain in production
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true // Allows cookies and authorization headers
-};
+// Depois de configurar as rotas da API, sirva os arquivos estáticos do React
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-// Servir o frontend React
-app.get('', (req, res) => {
+// Fallback para servir o frontend React em todas as outras rotas
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-
-app.use(cors(corsOptions));
-// Routes
-app.use('/api', itemRoutes);
+module.exports = app;
