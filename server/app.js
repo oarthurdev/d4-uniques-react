514c74335c58a74687b562decf578812bcb41ac6
Arthur Wagenknecht <oarthurdev@gmail.com>
2024-09-18 19:41:32 -0400

    Added vercej.json

--- server/app.js
+++ server/app.js
@@ -38,7 +38,7 @@ const corsOptions = {
 };
 
 // Servir o frontend React
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
