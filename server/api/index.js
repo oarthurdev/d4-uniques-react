3ea4eaa3381e7eb13e91fa0c5fee590f6a3a0f64
Arthur Wagenknecht <oarthurdev@gmail.com>
2024-09-18 20:35:45 -0400

    changes

--- server/api/index.js
+++
@@ -1,5 +0,0 @@
-const app = require('../app'); // Importa o app Express do seu arquivo app.js
-
-module.exports = (req, res) => {
-  app(req, res); // O Vercel trata cada chamada como uma função serverless
-};
