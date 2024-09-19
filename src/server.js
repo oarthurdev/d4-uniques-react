30c1c709cc9b78803a0bceb2e36a44532caf917e
Arthur Wagenknecht <oarthurdev@gmail.com>
2024-09-19 10:24:42 -0400

    teste

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const path = require('path');// Importando as rotas e lógica do app.js (backend)
+const path = require('path');
 
 const server = express();
 
 // Servir os arquivos estáticos do frontend React (build)
-server.use(express.static(path.join(__dirname, 'server/client/build')));
+server.use(express.static(path.join(__dirname, '../server/client/build')));
 
 // Fallback para servir o React em todas as rotas que não começarem com /api
 server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'server/client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../server/client/build', 'index.html'));
 });
 
 module.exports = server;
